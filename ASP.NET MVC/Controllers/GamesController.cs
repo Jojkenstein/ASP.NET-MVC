92a8f58cef6785a32e4e94da28dddbd675654e9b
Jojkenstein <klangjohan@gmail.com>
2022-11-10 11:35:29 +0100

    MVC Basics, Assignment 3 - Sessions and Cookies - The optional cookies part of the assignment is not included. The work progress of the optional part can be found in bransch TroubleShooting.

---
+++ ASP.NET MVC/Controllers/GamesController.cs
@@ -0,0 +1,64 @@
+﻿using ASP.NET_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_MVC.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult GuessNumber()
+        {
+            if (HttpContext.Session.GetString("randNr") == null)
+            {
+                HttpContext.Session.SetString("randNr", GamesModel.RandGen());
+                GamesModel.guessCnt = 0;
+                ViewBag.Set = "A new random number was generated!";
+                GamesModel.guessCorr = false; // Not sure if needed for special case yet
+                return View();
+            }
+                return View();
+        }
+
+        [HttpPost]
+        public IActionResult GuessNumber(string guessNr)
+        {
+            if (guessNr != null)
+            {
+                //Regenerate randNr in session if needed
+                if ((HttpContext.Session.GetString("randNr") == null) || (GamesModel.guessCorr == true))
+                {
+                    if (HttpContext.Session.GetString("randNr") == null)
+                    {
+                        HttpContext.Session.SetString("randNr", GamesModel.RandGen());
+                        GamesModel.guessCnt = 0;
+                        ViewBag.Reset = "A new random number was generated!";
+                        GamesModel.guessCorr = false; // Not sure if needed for special case yet
+                        return View();
+                    }
+                    HttpContext.Session.SetString("randNr", GamesModel.RandGen());
+                    GamesModel.guessCnt = 0;
+                    GamesModel.guessCorr = false;
+                }
+
+                GamesModel.guessCnt++;
+                HttpContext.Session.SetString("guessCnt", GamesModel.guessCnt.ToString());
+                ViewBag.Counter = $"Amount of guesses this rand: {GamesModel.guessCnt}";
+                int guessNrInt = Int32.Parse(guessNr);
+
+                // If for some reason randNr is null just create a new one from scratch.
+                // This is to get rid of warning CS8604 - Possible null reference argument for parameter.
+                int randNrSesInt = Int32.Parse(HttpContext.Session.GetString("randNr") ?? GamesModel.RandGen()); 
+                ViewBag.Result = GamesModel.EvalGuess(guessNrInt, randNrSesInt);
+                                
+                if (GamesModel.guessCorr == true)
+                {
+                    ViewBag.Reset = "A new random number will be generated.";
+                }
+            }
+            else
+            {
+                ViewBag.Result = "Your submit was empty. Please try again.";
+            }
+            return View();
+        }
+    }
+}
---
+++ ASP.NET MVC/Models/GamesModel.cs
@@ -0,0 +1,30 @@
+﻿namespace ASP.NET_MVC.Models
+{
+    public class GamesModel
+    {
+        public static bool guessCorr = false;
+        public static int guessCnt;
+        public List<string> highScore = new List<string>();
+        public static string EvalGuess(int guessNr, int randNr) //removed "static" for guessCorr to be able to be set
+        {
+            if (guessNr > randNr)
+            {
+                return $"{guessNr} is too high.";
+            }
+            else if (guessNr < randNr)
+            {
+                return $"{guessNr} is too low.";
+            }
+            else
+            {
+                GamesModel.guessCorr = true;
+                return $"{guessNr} is correct!";
+            }
+        }
+        public static string RandGen()
+        {
+            Random rand = new Random();
+            return rand.Next(1, 101).ToString();
+        }
+    }
+}
